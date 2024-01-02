b3435ad6cb84481249e9317f772975bbed3b4b31
SangWoo-Cho <155441493+san2u3u@users.noreply.github.com>
2024-01-02 20:25:56 +0900

    Time: 57 ms (10.23%), Space: 44.5 MB (20.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // 1. Brute Force
+        int len = nums.length;
+        for (int i = 0; i < len-1; i++) {
+            for (int j = i+1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
