71bb5133258850f5f7cf4ff7a31ee343ff0988af
Sangwoo Cho <155441493+san2u3u@users.noreply.github.com>
2024-01-13 23:48:03 +0900

    Time: 41 ms (6.01%), Space: 41.1 MB (12.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
+        // must use long (can be overflow)
         for(long i=1; i<=x; i++) {
             if(i*i == x) {
                 return (int)i;
